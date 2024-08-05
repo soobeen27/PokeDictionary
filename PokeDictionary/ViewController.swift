eb842d0d3c7f3a4aff9d6aaf6099b58ac446d92e
soobeen27 <youdew27@gmail.com>
2024-08-05 13:15:54 +0900

    Initial Commit

---
+++ PokeDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokeDictionary
+//
+//  Created by Soo Jang on 8/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
